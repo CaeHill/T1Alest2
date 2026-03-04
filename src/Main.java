af0f9e4501fb17b969e0debc6fa2c6827cc305ef
CaeHill <hillcaetano@gmail.com>
2026-03-04 13:54:57 -0300

    ideia inicial

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public static void main(String[] args){
+    int num = 15;
+    System.out.print(func(num));
+}
+
+public static int func (int origin) {
+
+    //Criar recursividade
+
+    return 0;
+}
